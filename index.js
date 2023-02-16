ea5fe3087c52fb8102072a02b0f8ab9b515d9f9c
Fernando Estrada <Estradafr@yahoo.com>
2023-02-15 23:46:58 -0800

    Adding home page view

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
 
+// View Engine
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 // Controller Route
 app.use('/restaurants', require('./controllers/restaurants'));
 
 // Home Page Route
 app.get('/', (req, res) => {
-	res.send('Home Page');
+	res.render('Home');
 });
 
 // 404 Page Route
