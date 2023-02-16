97869fe4ae2171a5df2447c71180f72e8f917fb0
Fernando Estrada <Estradafr@yahoo.com>
2023-02-16 11:12:20 -0800

    Adding and rendering a new view

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -23,4 +23,9 @@ router.get('/', (req, res) => {
 	res.render('restaurants/index', {restaurants});
 });
 
+// NEW
+router.get('/new', (req, res) => {
+	res.render('restaurants/new_restaurant');
+});
+
 module.exports = router;
---
+++ views/restaurants/new_restaurant.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+const Default = require('../default');
+
+function new_form() {
+	return (
+		<Default>
+			<main>
+				<h1>Add a new restaurant</h1>
+			</main>
+		</Default>
+	);
+}
+module.exports = new_form;
