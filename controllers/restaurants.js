96e60fce08a99ea815af43e14f43b76f98f820e4
Fernando Estrada <Estradafr@yahoo.com>
2023-02-16 00:04:56 -0800

    Adding restaurants view with mock data

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -3,7 +3,24 @@ const router = require('express').Router();
 
 // Restaurants Index Page
 router.get('/', (req, res) => {
-	res.render('restaurants/index');
+	let restaurants = [
+		{
+			name: 'H-Thai-ML',
+			city: 'Seattle',
+			state: 'WA',
+			cuisines: 'Thai',
+			pic: 'http://placekitten.com/250/250',
+		},
+		{
+			name: 'Coding Cat Cafe',
+			city: 'Las Vegas',
+			state: 'NV',
+			cuisines: 'Bakery',
+			pic: 'http://placekitten.com/250/250',
+		},
+	];
+
+	res.render('restaurants/index', {restaurants});
 });
 
 module.exports = router;
--- views/restaurants/index.jsx
+++ views/restaurants/index.jsx
@@ -1,11 +1,23 @@
 const React = require('react');
 const Default = require('../default');
 
-function index_page() {
+function index_page(data) {
+	let restaurantsFormatted = data.restaurants.map((restaurant) => {
+		return (
+			<div>
+				<h2>{restaurant.name}</h2>
+				<img
+					src={restaurant.pic}
+					alt={restaurant.name}
+				/>
+			</div>
+		);
+	});
 	return (
 		<Default>
 			<main>
-				<h1>INDEX PAGE</h1>
+				<h1>RESTAURANT INDEX PAGE</h1>
+				{restaurantsFormatted}
 			</main>
 		</Default>
 	);
