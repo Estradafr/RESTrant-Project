9fda5a9cae1622c2c5f69dbd1b785142e701a896
Fernando Estrada <Estradafr@yahoo.com>
2023-02-16 11:35:49 -0800

    Jazzed up form page

--- views/restaurants/new_restaurant.jsx
+++ views/restaurants/new_restaurant.jsx
@@ -10,48 +10,53 @@ function new_form() {
 					action="/restaurants"
 					method="POST"
 				>
-					<div>
+					<div className="form-group">
 						<label htmlFor="name">
 							Restaurant Name
 							<input
+								className="form-control"
 								id="name"
 								name="name"
 								required
 							/>
 						</label>
 					</div>
-					<div>
+					<div className="form-group">
 						<label htmlFor="pic">
 							Restaurant Picture
 							<input
+								className="form-control"
 								type="url"
 								id="pic"
 								name="pic"
 							/>
 						</label>
 					</div>
-					<div>
+					<div className="form-group">
 						<label htmlFor="city">
 							City
 							<input
+								className="form-control"
 								id="city"
 								name="city"
 							/>
 						</label>
 					</div>
-					<div>
+					<div className="form-group">
 						<label htmlFor="state">
 							State
 							<input
+								className="form-control"
 								id="state"
 								name="state"
 							/>
 						</label>
 					</div>
-					<div>
+					<div className="form-group">
 						<label htmlFor="cuisines">
 							Cuisines
 							<input
+								className="form-control"
 								id="cuisines"
 								name="cuisines"
 								required
@@ -59,6 +64,7 @@ function new_form() {
 						</label>
 					</div>
 					<input
+						className="btn btn-primary"
 						type="submit"
 						value="Add Restaurant"
 					/>
