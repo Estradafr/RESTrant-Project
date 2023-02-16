0731ecaa6c7fc0297a556851f874dfc8815c098e
Fernando Estrada <Estradafr@yahoo.com>
2023-02-15 23:01:20 -0800

    Added 404 page and dotenv

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Modules
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT;
 
 // Home Page Route
 app.get('/', (req, res) => {
-	res.send('hello');
+	res.status(404).send('TEST');
+});
+
+// 404 Page Route
+app.get('*', (req, res) => {
+	res.send('<h1>404</h1>');
 });
 
 // Listen
-app.listen(8080);
+app.listen(PORT);
